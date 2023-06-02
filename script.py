ce37db92fa8c9704c36944d48398db57bf3cac17
Javier Ibanez <Expertprep2015@gmail.com>
2023-06-02 02:05:48 -0400

    GUI example using Tkinter

--- script.py
+++ script.py
@@ -1,3 +1,16 @@
-favorite_num = 111
+import tkinter as tk
 
-print(favorite_num ** 3)
+# Create the main window
+window = tk.Tk()
+window.title("GUI Example")
+
+# Create a label widget
+label = tk.Label(window, text="Hello, GUI World!")
+label.pack()
+
+# Create a button widget
+button = tk.Button(window, text="Click Me!")
+button.pack()
+
+# Start the main event loop
+window.mainloop()
